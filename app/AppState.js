a31ab7710c8a9801b1f64c1f02b9e6477feb4d76
Shanker Karra <shankerkarra@gmail.com>
2021-07-15 15:41:34 -0600

    changes

--- app/AppState.js
+++ app/AppState.js
@@ -2,8 +2,15 @@ import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
 class AppState extends EventEmitter {
-
-
+  sportitems = [
+    new SportItems({
+      Item: 'Running Shoe',
+      Price: '120',
+      Description: 'Nike Sports running show',
+      Qty: '$200,000',
+      imgurl: "https://ssl.cdn-redfin.com/v374.3.1/images/merch/generalImages/CompleteSolution_Q1_2020_557_YardSign2.jpg"
+    })
+  ]
   Cash = 0
 
 }
